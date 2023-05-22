38f511807cb3c7220793c2a2344dbbf572a2653c
KhushMG <khushh.meet@gmail.com>
2023-05-22 16:21:27 -0700

    prog

--- ReversePolishNotationCalculator.java
+++ ReversePolishNotationCalculator.java
@@ -24,10 +24,14 @@ public class ReversePolishNotationCalculator {
 
         Scanner keyboard = new Scanner(System.in);
         char evalAgain = 'y';
+        Stack<Double> s = new Stack<>();
+        double operand1;
+        double operand2;
+        double result;
 
         ShouldWeTryAgain: while (evalAgain == 'y') {
             double nextNumber = 0;
-
+            String operator = "";
             System.out.println("\nRPN calculator");
             System.out.println("\tSupported operators: + - * /");
             System.out.print("Type your RPN expression in so that it can be evaluated: ");
@@ -43,82 +47,170 @@ public class ReversePolishNotationCalculator {
             }
 
             Scanner scExpr = new Scanner(rpnExpr + "\n");
-            /* while() */ { // repeat the following while there's another token left in the Scanner:
+            while (scExpr.hasNext()) { // repeat the following while there's another token left in the Scanner:
                 if (explain) {
-                    scExpr = printRemainingExpression(null, scExpr);
+                    scExpr = printRemainingExpression(s, scExpr);
                 }
                 // if the next thing in the expression is a number:
-                if (explain) {
-                    System.out.println("\tPushing " + nextNumber + " onto the stack of operands (numbers)");
-                }
+                if (scExpr.hasNextDouble()) {
+                    nextNumber = scExpr.nextDouble();
+                    if (explain) {
+                        System.out.println("\tPushing " + nextNumber + " onto the stack of operands (numbers)");
+                        s.push(nextNumber);
+                        System.out.println(s.toString());
+                    } else {
+                        s.push(nextNumber);
+                    }
 
-                // if the next thing in the expression is not a number then we'll assume it's an
-                // operator
-                // (unless we find out that it's not a supported operator)
-
-                System.err.println(
-                        "ERROR! Operator (non-numeric input) contains more than 1 character: " + "PLACEHOLDER");
-                System.out.println(
-                        "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
-                // continue ShouldWeTryAgain; // This line will jump back to the outer loop
-
-                // // "4 3 -" should be +1, not -1
-                // // When parsing the expression 4 is pushed first, then 3
-                // // the second operand (the 3) is at the top (so we'll pop that into operand2)
-                // // then pop the first operand (the 4) into operand1
-                // if ( /* the stack of operands isEmpty */) {
-                //     System.err.println(
-                //             "ERROR! Expected to find 2 operands (numbers) but we don't have any numbers on the stack!");
-                //     System.out.println(
-                //             "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
-                //     continue ShouldWeTryAgain;
-                // }
-                // // operand2 = /* get the top operand off the number stack */
-
-                // if ( /* the stack of operands isEmpty */) {
-                //     System.err.println(
-                //             "ERROR! Expected to find 2 operands (numbers) but we don't have a second number on the stack!");
-                //     System.out.println(
-                //             "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
-                //     continue ShouldWeTryAgain;
-                // }
-                // // operand1 = /* get the top operand off the number stack */
-
-                switch (' ') {
-                    default:
-                        System.err.println("ERROR! Operator not recognized: " + "PLACEHOLDER");
+                } else {
+                    operator = scExpr.next();
+                    if (operator.length() > 1) {
+                        System.err.println(
+                                "ERROR! Operator (non-numeric input) contains more than 1 character: " + operator);
                         System.out.println(
                                 "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
-                        // continue ShouldWeTryAgain; // This line will jump back to the outer loop
-                }
-                if (explain)
-                    System.out.println("\tPopping " + "PLACEHOLDER" + " and " + "PLACEHOLDER" + " then applying "
-                            + "PLACEHOLDER" + " to them, then pushing the result back onto the stack");
+                        continue ShouldWeTryAgain; // This line will jump back to the outer loop
+                    }
+                    if (s.isEmpty()) {
+                        System.err.println(
+                                "ERROR! Expected to find 2 operands (numbers) but we don't have any number on the stack!");
+                        System.out.println(
+                                "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
+                        continue ShouldWeTryAgain;
+                    }
+                    /* get the top operand off the number stack */
+                    operand2 = s.pop();
+                    if (s.isEmpty()) {
+                        System.err.println(
+                                "ERROR! Expected to find 2 operands (numbers) but we don't have a second number on the stack!");
+                        System.out.println(
+                                "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
+                        continue ShouldWeTryAgain;
+                    }
+                    /* get the top operand off the number stack */
+                    operand1 = s.pop();
 
+                    switch (operator) {
+                        default:
+                            System.err.println("ERROR! Operator not recognized: " + operator);
+                            System.out.println(
+                                    "Since we can't evaluate that expression we'll ask you for another one to evaluate instead");
+                            continue ShouldWeTryAgain; // This line will jump back to the outer loop
+                        case "+":
+                            result = operand2 + operand1;
+                            s.push(result);
+                            break;
+                        case "-":
+                            result = operand1 - operand2;
+                            s.push(result);
+                            break;
+                        case "/":
+                            result = operand1 / operand2;
+                            s.push(result);
+                            break;
+                        case "*":
+                            result = operand2 * operand1;
+                            s.push(result);
+                            break;
+
+                    }
+                    if (explain) {
+                        System.out.println("\tPopping " + operand1 + " and " + operand2 + " then applying "
+                                + operator + " to them, then pushing the result back onto the stack");
+                    }
+                }
             }
+            // if the next thing in the expression is not a number then we'll assume it's an
+            // operator
+            // (unless we find out that it's not a supported operator)
+            // // "4 3 -" should be +1, not -1
+            // // When parsing the expression 4 is pushed first, then 3
+            // // the second operand (the 3) is at the top (so we'll pop that into operand2)
+            // // then pop the first operand (the 4) into operand1
+            // if (s.isEmpty()) {
+            // System.err.println(
+            // "ERROR! Expected to find 2 operands (numbers) but we don't have any number on
+            // the stack!");
+            // System.out.println(
+            // "Since we can't evaluate that expression we'll ask you for another one to
+            // evaluate instead");
+            // continue ShouldWeTryAgain;
+            // }
+            // /* get the top operand off the number stack */
+            // operand2 = s.pop();
+            // if (s.isEmpty()) {
+            // System.err.println(
+            // "ERROR! Expected to find 2 operands (numbers) but we don't have a second
+            // number on the stack!");
+            // System.out.println(
+            // "Since we can't evaluate that expression we'll ask you for another one to
+            // evaluate instead");
+            // continue ShouldWeTryAgain;
+            // }
+            // /* get the top operand off the number stack */
+            // operand1 = s.pop();
+
+            // switch (operator) {
+            // default:
+            // System.err.println("ERROR! Operator not recognized: " + operator);
+            // System.out.println(
+            // "Since we can't evaluate that expression we'll ask you for another one to
+            // evaluate instead");
+            // continue ShouldWeTryAgain; // This line will jump back to the outer loop
+            // case "+":
+            // result = operand2 + operand1;
+            // s.push(result);
+            // break;
+            // case "-":
+            // result = -1 * (operand2 - operand1);
+            // s.push(result);
+            // break;
+            // case "/":
+            // result = operand2 / operand1;
+            // s.push(result);
+            // break;
+            // case "*":
+            // result = operand2 * operand1;
+            // s.push(result);
+            // break;
+
+            // }
+            // if (explain)
+            // System.out.println("\tPopping " + operand1 + " and " + operand2 + " then
+            // applying "
+            // + operator + " to them, then pushing the result back onto the stack");
 
             // At this point we've finished reading through the expression
 
             // If there's more than 1 operand (number) left then we print this error
             // message:
-            System.err.println("ERROR! Ran out of operators before we used up all the operands (numbers):");
-            // Go through all the operands an print them out:
-            System.err.println("\t" + "PLACEHOLDER");
+            int size = s.size();
+            if (s.size() > 1) {
+                System.err.println("ERROR! Ran out of operators before we used up all the operands (numbers):");
+                // Go through all the operands an print them out:
+                for (int i = 0; i < size; i++) {
+                    System.err.println("\t" + s.pop());
+                }
+            }
 
-            // If there wasn't any operands then print out this error message:
-            System.err.println("ERROR! Ran out of operands (numbers)");
+            else if (s.size() == 1) {
+                System.out.println("END RESULT: " + s.pop());
+            }
 
-            // If there's exactly 1 operand then it must be the answer
-            System.out.println("END RESULT: " + "PLACEHOLDER");
+            else if (s.isEmpty()) {
+                System.err.println("ERROR! Ran out of operands (numbers)");
+            }
 
             System.out
                     .print("\nWould you like to evaluate another expression? (y or Y for yes, anything else to exit) ");
+            s.clear();
             String repeat = keyboard.nextLine().trim().toLowerCase();
             if (repeat.length() == 1 && repeat.charAt(0) == 'y')
                 evalAgain = 'y';
-            else
+            else {
                 evalAgain = 'n';
+            }
+            System.out.println("Thank you for using RPN Calculator!");
         }
-        System.out.println("Thank you for using RPN Calculator!");
     }
 }
